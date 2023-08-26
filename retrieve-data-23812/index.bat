9502b18c2c2afae356ca994c7e9b0e12ee161d7c
ibtisam midlet <storebitid@gmail.com>
2023-08-26 21:37:51 +0100

    *

--- retrieve-data-23812/index.bat
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-nvm use 18.12.1
-timeout /t 10
-node index.js
--- retrieve-data-23812/ssl.js
+++
@@ -1,8 +0,0 @@
-
-const Express = require('express'); // Import express
-
-const app = require('express')();
-const https = require('https');
-const fs = require('fs');
-app.get('/', (req, res) => {    res.send('Hello World');});
-https.createServer({    key: fs.readFileSync('./ssl/biotunekey.pem'),    cert: fs.readFileSync('./ssl/cert.pem')}, app).listen(443);
