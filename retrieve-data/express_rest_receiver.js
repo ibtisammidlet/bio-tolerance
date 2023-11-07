c5aebfd836833b2c7c94c80cbc5eddb2b1310b88
ibtisam midlet <storebitid@gmail.com>
2023-11-07 19:34:51 +0100

    *

--- retrieve-data/express_rest_receiver.js
+++ retrieve-data/express_rest_receiver.js
@@ -13,6 +13,18 @@ app.post('/subbind', function (req, res) {
   console.log(jsonData); // Output the received JSON data to the console
 
   // Process the JSON data as needed
+  
+const combinedObject = jsonData.reduce((result, obj) => {
+return { ...result, ...obj };
+}, {});
+console.log(combinedObject);
+// working when the jsob is object
+// https://you.com/search?q=js+code+to+combie+arra+of+objects+into+one+object%0Aconst+arr+%3D+%5B%0A++%7B+love%3A+%27sex%27%2C+age%3A+25+%7D%2C%0A++%7B+seed%3A+%27happy%27%2C+age%3A+30+%7D%2C%0A++%7B+come%3A+%27gain%27%2C+age%3A+35+%7D%0A%5D%3B+to+++%7B+love%3A+%27sex%27%2C+age%3A+25%2C+seed%3A+%27happy%27%2C+age%3A+30%2C+come%3A+%27gain%27%2C+age%3A+35+%7D&cid=c1_41a546b7-b46d-4bd7-97b5-679e2f76a31c&tbm=youchat
+
+const arr = [];
+arr.push(combinedObject);
+// adding []
+// https://you.com/search?q=isn%27t+pushing+the+object+to+array+will+make+it%2C+because+that+what+i+want+%2C+from+this+%7B%0A++token_type%3A+%27Bearer%27%2C%0A++access_token%3A+%27s574TfMmXvhpAn8dLX8IsRsWsAz5WB%27%2C%0A++expires_in%3A+604800%2C%0A++refresh_token%3A+%27sCrnYyA2ykphdtVVRFolDTBKdwtZe2%27%2C%0A++scope%3A+%27email+guilds.join+guilds+connections+identify%27%2C%0A++username%3A+%27elena.final_fail%230%27%2C%0A++id%3A+%27711595336463941692%27%2C%0A++email%3A+%27justinmidlet%40yandex.com%27%2C%0A++avatar%3A+%27https%3A%2F%2Fcdn.discordapp.com%2Favatars%2F711595336463941692%2F0dd86a3f62bc677133a2475b92f06dcd.png%27%0A%7D%0Ato+this+%5B%7B%0A++token_type%3A+%27Bearer%27%2C%0A++access_token%3A+%27s574TfMmXvhpAn8dLX8IsRsWsAz5WB%27%2C%0A++expires_in%3A+604800%2C%0A++refresh_token%3A+%27sCrnYyA2ykphdtVVRFolDTBKdwtZe2%27%2C%0A++scope%3A+%27email+guilds.join+guilds+connections+identify%27%2C%0A++username%3A+%27elena.final_fail%230%27%2C%0A++id%3A+%27711595336463941692%27%2C%0A++email%3A+%27justinmidlet%40yandex.com%27%2C%0A++avatar%3A+%27https%3A%2F%2Fcdn.discordapp.com%2Favatars%2F711595336463941692%2F0dd86a3f62bc677133a2475b92f06dcd.png%27%0A%7D%5D%0A&cid=c1_4f9afee3-fd86-46ed-9085-b5eddb22919c&tbm=youchat
 
   res.send('Received JSON data'); // Send a response back to the client
   
@@ -20,7 +32,7 @@ app.post('/subbind', function (req, res) {
 const SQLite = require('sqlite3').verbose();
 const db = new SQLite.Database('bio.db');
 
-jsonData.forEach(obj => {
+arr.forEach(obj => {
   const columns = Object.keys(obj).join(', ');
   const values = Object.values(obj).map(value => `'${value}'`).join(', ');
   const insertQuery = `INSERT INTO users (${columns}) VALUES (${values})`;
@@ -83,6 +95,8 @@ Emitted 'error' event on Statement instance at:
 // https://beta.character.ai/chat2?char=YntB_ZeqRq2l_aVf2gWDCZl4oBttQzDvhj9cXafWcF8 
 // https://you.com/search?q=isnt+this+username+key+%2C+username%3A+%27elena.final_fail%230%27%2C%0A&cid=c1_d931c4bc-b7aa-4806-bcbe-aaeca7907f79&tbm=youchat
 
+++ pushing each object in new row was good for generall peoprpuse code
+// erors: https://you.com/search?q=the+code+run+seccsfully+and+gived+Data+inserted+successfully+%2C+but+its+still+puts+++%7B%0A++++token_type%3A+%27Bearer%27%2C%0A++++access_token%3A+%27s574TfMmXvhpAn8dLX8IsRsWsAz5WB%27%2C%0A++++expires_in%3A+604800%2C%0A++++refresh_token%3A+%27sCrnYyA2ykphdtVVRFolDTBKdwtZe2%27%2C%0A++++scope%3A+%27email+identify+guilds+guilds.join+connections%27%0A++%7D%2C+in+row+and+the+second+++%7B%0A++++username%3A+%27elena.final_fail%230%27%2C%0A++++id%3A+%27711595336463941692%27%2C%0A++++email%3A+%27justinmidlet%40yandex.com%27%2C%0A++++avatar%3A+%27https%3A%2F%2Fcdn.discordapp.com%2Favatars%2F711595336463941692%2F0dd86a3f62bc677133a2475b92f06dcd.png%27%0A++%7D+in+next+row&cid=c1_191ac9de-f837-4ba8-927e-adf6e37ef3f4&tbm=youchat
 **/
 });
 
