f5b04573352f59f3ebe61111a2e98220fc30ec59
ibtisam midlet <storebitid@gmail.com>
2023-11-08 17:15:50 +0100

    *

--- retrieve-data/index.js
+++ retrieve-data/index.js
@@ -5,6 +5,11 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(443, () => {
-  console.log('Server is running on port 443');
+app.listen(80, () => {
+  console.log('Server is running on port 80');
 });
+
+/** The port number is hardcoded as 80
+require('./express_rest_receiver.js');
+require('./oauth.js');
+**/ 
