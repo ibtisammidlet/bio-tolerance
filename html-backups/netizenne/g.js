e828bd7a15371541a22298e88b7a5d1969d17932
ibtisam midlet <storebitid@gmail.com>
2023-11-18 08:54:05 +0100

    *

--- html-backups/netizenne/g.js
+++ html-backups/netizenne/g.js
@@ -23,28 +23,10 @@ var htmlscale = windowwidth / wantedwidth ;
 var windowheight = 1/htmlscale*100;
 
 //* setting html width as the wanted width and scale the page by the result of htmlscale var
-var htmlstyle = '<style>html {width: '+wantedwidth+'px  !important; height: '+windowheight+'%; transform-origin: 0px 0px 0px !important; }</style>';
+var htmlstyle = '<style>html {width: '+wantedwidth+'px  !important; height: '+windowheight+'%; transform-origin: 0px 0px 0px !important; transform: translate(0px, 0px) scale(' + htmlscale + ');}</style>';
 
 //* injecting css if window width is less then wanted width
 if(windowwidth > 0 && windowwidth <= wantedwidth-1) {$('head').append(htmlstyle+extracss);};
 });
 
-/**
-$( document ).ready(function() {
 
-//* var of wantedwidth can be tuned
-var wantedwidth = 768;
-
-//* getting window width
-var windowwidth = $(window).width();
-
-//* devid window width by wanted width
-var htmlscale = windowwidth / wantedwidth ;
-
-//* calc new height
-var newheight = "<style>embed{min-height: "+1/htmlscale*100+"%;}</style>";
-
-//* injecting css if window width is less then wanted width
-if(windowwidth > 0 && windowwidth <= wantedwidth-1) {$('head').append(newheight);};
-});
-**/
