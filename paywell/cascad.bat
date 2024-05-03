c332fef13efeb16b419df103d093238ac255e020
ibtisam midlet <storebitid@gmail.com>
2024-05-03 22:50:31 +0100

    c

--- paywell/cascad.bat
+++ paywell/cascad.bat
@@ -10,6 +10,10 @@ start cmd /c "E:\data\development\nodejs\node_modules\biotune\frontend\start.bat
 timeout /t 60
 
 
+start cmd /c "E:\data\development\nodejs\node_modules\biotune\frontend\gumroad.js.adding_new_line.js.bat"
+timeout /t 60
+
+
 cd ..
 cd ..
 cd bio-tolerance\paywell
